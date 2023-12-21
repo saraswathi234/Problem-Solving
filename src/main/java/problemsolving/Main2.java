28af4b9d2ec9f3a53b011ae27d3faff5baf300db
saraswathi234 <116253625+saraswathi234@users.noreply.github.com>
2023-12-21 12:02:51 +0530

    day01 problem solving
    I have completed l1 problem solving

---
+++ src/main/java/problemsolving/Main2.java
@@ -0,0 +1,16 @@
+package problemsolving;
+
+
+public class Main2 {
+	    public static void main(String[] args) {
+	        String a = "2 5";
+	        String b = "2 5 6";
+	        String c = "2 4 5";
+
+	        System.out.println(a);
+	        System.out.println(b);
+	        System.out.println(c);
+	    }
+	}
+
+
